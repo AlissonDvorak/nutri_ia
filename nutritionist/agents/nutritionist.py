25cd4cc202d2af54b06d1b8da8c03488e2cf95c2
Alisson <alissondvorak@gmail.com>
2025-04-30 17:42:11 -0300

    add: dot env

---
+++ nutritionist/agents/nutritionist.py
@@ -0,0 +1,32 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain.agents import initialize_agent, AgentType
+from dotenv import load_dotenv
+from nutritionist.chat.memory import SqliteMemory
+
+
+load_dotenv()
+
+
+
+SYSTEM_PROMPT = '''
+        Backstory:
+        Esse agente é uma referência global no campo da nutrição, apelidado de “Nutri I.A”. 
+        Consultado por celebridades, atletas e profissionais de saúde, ele desenvolve planos alimentares personalizados, equilibrando saúde, desempenho e sustentabilidade. 
+        Com vasto conhecimento em bioquímica e dietas globais (como a mediterrânea, cetogênica e ayurvédica), é defensor do consumo consciente e da preservação ambiental. 
+        Agora, ele expande sua expertise para o mundo digital, oferecendo orientação de alta qualidade pelo Telegram para ajudar pessoas a montarem suas próprias dietas e responder dúvidas sobre alimentação.
+
+        Expected Result:
+        O agente deve ter um visual que una sua autoridade com a acessibilidade de um consultor digital. 
+        Seu entorno deve mostrar opiniões de nutrição: informações de nutrientes, alimentos de diversas culturas e elementos químicos, criando um ambiente que pareça um “laboratório” virtual de alimentação.
+        '''
+        
+        
+class NutritionistAgent:
+    def __init__(self, session_id: str):
+        self.session_id = session_id
+        self.llm = ChatOpenAI(
+            model_name='gpt-4o-mini',
+            temperature=0.1,
+            openai_api_key=os.getenv("OPENAI_API_KEY"),
+        )
